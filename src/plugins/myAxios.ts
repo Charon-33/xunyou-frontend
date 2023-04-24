8ae4688be010d9f4fd4f34e95edcf5a40eb5ef41
charon <1935496654@qq.com>
2023-04-24 13:29:33 +0800

    最终版2.1 登录会回退

--- src/plugins/myAxios.ts
+++ src/plugins/myAxios.ts
@@ -26,7 +26,7 @@ myAxios.interceptors.response.use(function (response) {
 
     // 未登录则跳转到登录页
     if (response?.data?.code === 40100) {
-        const redirectUrl = window.location.href;
+        // const redirectUrl = window.location.href;
         // window.location.href = `/user/login?redirect=${redirectUrl}`;
         window.location.href = '/user/login';
     }
